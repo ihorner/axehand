f94a56762358bf68002cc35958cdd866caf02dad
Ian Horner <ihorner@tiltyhouse.com>
2015-05-07 18:58:55 -0700

    Added things to the README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# axehand
+# Welcome to Axehand
+Axehand is a custom droplet for automamating the removal of commercial breaks from LoadingReadyLive videos. 
+
+# Usage
+Drag a single mp4 file and it's corresponding text file pair onto the Axehand icon. Let it do its magic. If it doesn't work, there's probably something wrong with either of the files and it will have to be processed manually.
+
+# Requirements
+- ffmpeg – used for actually processing the files
+- Paul's crazy server system
+
+# Upcoming Features
+- Multiple file pair drop compatability
+- Helpful error reporting
+
+# Support
+axehand@tiltyhouse.com
+
+# Watch Our Stuff
+Watch us stream live at [LoadingReadyLive](http://www.twitch.tv/loadingreadyrunr)
+Watch our other stuff at [LoadingReadyRun](http://loadingreadyrun.com)
+Support us at [Patreon.com/LoadingReadyRun](http://patreon.com/loadingreadyrun)
--- axehand.rb
+++ axehand.rb
@@ -1,27 +1,26 @@
 #!/usr/bin/ruby
 
-
 textfile = ARGV[0]
 movie = ARGV[1]
 
-# textfile = "2015-01-21_120323114.txt"
-# movie = "2015-01-21_120323114.ts"
+moviefull = movie.gsub("\ ", "\\\ ")
+textfull = textfile.gsub("\ ", "\\\ ")
 
 edl = []
-moviename = movie[0..-5]
+moviename = File.basename(movie, ".mp4")
 tempfiles = []
 cuts = []
 
+directory = File.dirname(movie)
+directory2 = File.dirname(movie)
+directory2["\ "]="\\\ "
+
 # Load text files of timecodes and sort
-File.readlines(textfile).sort_by(&:to_i).each do |f|
+File.readlines(textfile, encoding: 'UTF-8').sort_by(&:to_i).each do |f|
 	edl << f.strip.split(/:/)
 end
 
 z = edl.length
-
-# cuts.push (5)
-
-# edl[0][0] = edl[0][0].to_i-50000000
 
 # Add 5s Buffer to OP, Commercial and ED
 edl.each_with_index { |val, index|
@@ -39,51 +38,35 @@ end
 
 ffcutpoints = cuts.join(',')
 
-
 # Write out new keyframes at cut points and Cut the video into pieces, this is my last resort
-system "ffmpeg -i #{movie} -force_key_frames #{ffcutpoints} -c copy -map 0 \
- -f segment -segment_list out.ffcat -segment_times #{ffcutpoints} -segment_time_delta 0.05 -bsf:v h264_mp4toannexb #{moviename}.%03d.ts"
+system "/usr/local/bin/ffmpeg -i #{moviefull} -force_key_frames #{ffcutpoints} -c copy -map 0 \
+ -f segment -segment_list #{directory2}/out.ffcat -segment_times #{ffcutpoints} -segment_time_delta 0.05 -bsf:v h264_mp4toannexb #{directory2}/#{moviename}.%03d.ts"
 
-# Remove first segment to concatlist.ffcat and tempfiles array
-		# File.open("concatlist.ffcat", 'a') { |f| 
-		# 	f.puts "file #{moviename}.000.ts"
-		# }
-
-tempfiles.push("#{moviename}.000.ts")
+tempfiles.push("#{directory}/#{moviename}.000.ts")
 
 # Add non-commercial segments to concatlist amd tempfiles array
 edl.each_with_index { |val, index|
 	indexx = index+1
 	if edl[index][1] != "Commercial" and edl[index][1] != "Going Online" and edl[index][1] != "Going Offline" and index+1 < z
-		File.open("concatlist.ffcat", 'a') { |f| 
-			f.puts "file #{moviename}.#{"%03d" % indexx}.ts"
+		File.open("#{directory}/concatlist.ffcat", 'a') { |f| 
+			f.puts "file #{directory2}/#{moviename}.#{"%03d" % indexx}.ts"
 		}
-		tempfiles.push("#{moviename}.#{"%03d" % indexx}.ts")
+		tempfiles.push("#{directory}/#{moviename}.#{"%03d" % indexx}.ts")
 	else
-		tempfiles.push("#{moviename}.#{"%03d" % indexx}.ts")
+		tempfiles.push("#{directory}/#{moviename}.#{"%03d" % indexx}.ts")
 	end
 }
 
 endtime = edl[z-1][0].to_f/10000000
 
-# Add last segment to concatlist
-# if edl[z][1] = "Going Offline"
-# 	tempfiles.push("#{moviename}.#{"%03d" % z}.ts")
-# else
-# 	File.open("concatlist.ffcat", 'a') { |f| 
-# 			f.puts "file #{moviename}.#{"%03d" % z}.ts"
-# }
-# tempfiles.push("#{moviename}.#{"%03d" % z}.ts")
-# end
-
 # Stitch it all together
-system "ffmpeg -f concat -i concatlist.ffcat -bsf:a aac_adtstoasc -c copy #{moviename}.cut.mp4"
+system "/usr/local/bin/ffmpeg -f concat -i #{directory2}/concatlist.ffcat -bsf:a aac_adtstoasc -c copy #{directory2}/#{moviename}.cut.mp4"
 
 # Add concatlists to tempfiles array
-tempfiles.push("concatlist.ffcat")
-tempfiles.push("out.ffcat")
+tempfiles.push("#{directory}/concatlist.ffcat")
+tempfiles.push("#{directory}/out.ffcat")
 
-Remove temp files
+# Remove temp files
 tempfiles.each { |x| 
 File.delete(x)
 }
